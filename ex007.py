d42499152126a372352db3d22684fb24aff52516
Elisariane <elisarianeBS@gmail.com>
2024-01-09 18:14:37 -0300

    :sparkles: feat: Add solução exercicio 007

---
+++ ex007.py
@@ -0,0 +1,6 @@
+number1 = float(input('Digite a primeira nota: '))
+number2 = float(input('Digite a segunda nota: '))
+
+average = (number1+number2)/2
+
+print(f'A média do aluno é {round(average, 2)}')
