dbd2613c594cfd0bf6ff947727ab42353fe0863f
Elisariane <elisarianeBS@gmail.com>
2024-01-09 18:09:28 -0300

    :sparkles: feat: Add solução exercicio 006

---
+++ ex006.py
@@ -0,0 +1,7 @@
+import math
+
+number = int(input('Digite um número: '))
+
+print(f'O dobro de {number} é {number*2}')
+print(f'O triplo de {number} é {number*3}')
+print(f'A raiz quadrada de {number} é {round(math.sqrt(number), 2)}')
